54cf4113d41a339d13deec1325387e16c9510bc1
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-12-18 19:11:00 +0330

    Handles role checking

--- src/Helper.php
+++ src/Helper.php
@@ -13,8 +13,8 @@ class Helper
      * @return boolean      
      */
     public static function isTenant($user): bool
-    {
-        return ! is_null($user->loadMissing('roles')->roles->find(Registration::option('tenant_role')));
+    { 
+        return static::has($user, 'tenant_role'); 
     }
 
     /**
@@ -24,11 +24,57 @@ class Helper
      * @return static      
      */
     public static function ensureIsTenant($user)
+    { 
+        return static::ensureHas($user, 'tenant_role');
+    }
+
+    /**
+     * Determine if the given user is Landlord.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Model  $user
+     * @return boolean      
+     */
+    public static function isLandlord($user): bool
     {
-        if(! static::isTenant($user) && $roleId = intval(Registration::option('tenant_role'))){
+        return static::has($user, 'landlord_role');
+    }
+
+    /**
+     * Ensure the given user is be a Landlord.
+     *  
+     * @param  \Illuminate\Database\Eloquent\Model  $user
+     * @return static      
+     */
+    public static function ensureIsLandlord($user)
+    {
+        return static::ensureHas($user, 'landlord_role');
+    }
+
+    /**
+     * Determine if the given user has the given role.
+     * 
+     * @param   string $role
+     * @param  \Illuminate\Database\Eloquent\Model  $user
+     * @return boolean      
+     */
+    public static function has($user, $role): bool
+    {
+        return ! is_null($user->loadMissing('roles')->roles->find(Registration::option($role)));
+    }
+
+    /**
+     * Ensure the given user has the given lord.
+     *
+     * @param   string $role
+     * @param  \Illuminate\Database\Eloquent\Model  $user
+     * @return static      
+     */
+    public static function ensureHas($user, $role)
+    {
+        if(! static::isTenant($user) && $roleId = intval(Registration::option($role))){
             $user->roles()->syncWithoutDetaching($roleId);
         }  
 
         return new static;
-    }
+    } 
 }
