36a0763b95ebd8bd69f8018a6be8e69b5cc3105c
Ismail Zare <zarehesmaiel@gmail.com>
2021-02-28 11:00:18 +0330

    Fixes owner relationship eager loading

--- src/Models/AuthorizableModel.php
+++ src/Models/AuthorizableModel.php
@@ -20,4 +20,17 @@ class AuthorizableModel extends Model implements Authorizable
 	{
 		return $this->auth();
 	}
+
+    /**
+     * Get a relationship.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getRelationValue($key)
+    {  
+    	return parent::getRelationValue(
+    		$key == 'owner' && ! $this->relationLoaded('owner') ? 'auth' : $key
+    	);
+    }
 }
